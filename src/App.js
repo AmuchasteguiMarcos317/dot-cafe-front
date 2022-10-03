825e92d42382787a40e82ecaa62d69536809867a
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-10-03 13:34:34 -0300

    Component, first page created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CafeLayout from './Layouts/CafeLayout';
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <CafeLayout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </CafeLayout>
+      </BrowserRouter>
+    </>
   );
 }
 
