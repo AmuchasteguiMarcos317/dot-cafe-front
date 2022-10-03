21af66f1eebd5279dcf6d7f11e4c6b9e8722f59e
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-10-03 19:55:10 -0300

    Header created

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,56 +3,72 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Header from '../Styles/Header.css'
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Header from '../Styles/Header.css';
+
 
 export default function header() {
     return (
         <>
-            <div className='headerContain'>
-                <div className='header'>
-                    <img src='https://cdn.discordapp.com/attachments/1009863821361434774/1026528237960052857/png-transparent-coffee-logo-cafe-photography-logo_1.png' alt='image' />
-                    <p>. Café</p>
-                </div>
-            </div>
-            <Navbar className='navbarContain' bg="warning" expand="lg">
-                <Container className='dropdown'>
-                    <Navbar.Brand href="#home">. Café</Navbar.Brand>
+            <Container fluid className='headerContain'>
+                <Container fluid className='header'>
+                    <img src='logo1.png' alt='image' />
+                    <Navbar.Brand className='navText' href="#home"><span></span> </Navbar.Brand>
+                    <p>Pedi tu<span className='headerSpan'> .</span>Café</p>
+                </Container>
+            </Container>
+            <Navbar className='navbarContain'>
+                <Container fluid className='dropdown'>
+                    <Container>
+                        <Navbar.Brand className='navText' href="#home"><span></span> </Navbar.Brand>
+                    </Container>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <NavDropdown title="Menú" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown title="Café" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
+                            <NavDropdown title="Granel" id="basic-nav-dropdown">
+                                <NavDropdown.Item className='navDrop' variant='primary' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415916.png' />1 Kg.</NavDropdown.Item>
+                                <NavDropdown.Item className='navDrop' href="#action/3.2"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415916.png' />500 Grs.</NavDropdown.Item>
+                                <NavDropdown.Item className='navDrop' href="#action/3.3"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415902.png' />250 Grs.</NavDropdown.Item>
                             </NavDropdown>
                             <NavDropdown title="Máquinas" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
+                                <NavDropdown.Item className='navDrop' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415865.png' alt='icon' /> Cápsula</NavDropdown.Item>
+                                <NavDropdown.Item className='navDrop' href="#action/3.2"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415920.png' alt='icon' />
+                                    Italiana
                                 </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
+                                <NavDropdown.Item className='navDrop' href="#action/3.3"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/1269/1269081.png' alt='icon' /> Barista</NavDropdown.Item>
+                                <NavDropdown.Item className='navDrop' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/672/672449.png' alt='icon' /> Con filtro</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Accesorios" id="basic-nav-dropdown">
+                                <NavDropdown.Item className='navDrop' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/6415/6415935.png' alt='icon' /> Tazas</NavDropdown.Item>
+                                <NavDropdown.Item className='navDrop' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/2740/2740800.png' alt='icon' /> Filtros sustentables</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Gif Cards" id="basic-nav-dropdown">
+
+                                <NavDropdown.Item className='navDrop' href="#action/3.1"><img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/4965/4965109.png' alt='icon' /> $1.000</NavDropdown.Item>
+
+                                <NavDropdown.Item className='navDrop' href="#action/3.2">
+                                    <img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/4965/4965109.png' alt='icon' /> $2.000
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className='navDrop' href="#action/3.3">
+                                    <img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/4965/4965109.png' alt='icon' /> $5.000
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className='navDrop' href="#action/3.4">
+                                    <img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/4965/4965109.png' alt='icon' /> $10.000
+                                </NavDropdown.Item>
+
+                            </NavDropdown>
+                            <NavDropdown title="Reciclaje" id="basic-nav-dropdown">
+                                <NavDropdown.Item className='navDrop' href="#action/3.1">
+                                    <img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/3299/3299935.png' alt='icon' /> Punto de reciclaje</NavDropdown.Item>
+
+                                <NavDropdown.Item href="#action/3.2">
+                                    <img className='headerIcon' src='https://cdn-icons-png.flaticon.com/512/3299/3299853.png' alt='icon' />
+                                    Campaña de reciclaje
                                 </NavDropdown.Item>
                             </NavDropdown>
+                            <Navbar.Brand href="#home"><span className='navAll'>Ver todo</span></Navbar.Brand>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
