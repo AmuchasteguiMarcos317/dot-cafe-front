5df2730bce22093ffdd2cd597735ecb7148aade7
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-10-03 19:48:57 -0300

    welcome and cards added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,15 @@
 import { Button } from 'react-bootstrap'
 import React from 'react'
+import HomeCards from '../Components/HomeCards'
+import Welcome from '../Components/Welcome'
 
 export default function Home() {
     return (
         <>
-            <div>Home</div>
+            <Welcome />
+            <main>
+                <HomeCards />
+            </main>
         </>
     )
 }
