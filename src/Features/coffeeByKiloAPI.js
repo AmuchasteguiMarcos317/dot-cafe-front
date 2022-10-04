a46853b36d5fa14545b240039ab390cbea4e9d40
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-10-04 19:55:56 -0300

    routes fixed

--- src/Features/coffeeByKiloAPI.js
+++ src/Features/coffeeByKiloAPI.js
@@ -10,7 +10,7 @@ export const coffeeByKiloAPI = createApi({
     
     endpoints: (builder) => ({
         getAllCoffeeByKilo: builder.query({
-            query: () => '/kits'
+            query: () => '/coffee-kilogram'
         }),
     })
 })
