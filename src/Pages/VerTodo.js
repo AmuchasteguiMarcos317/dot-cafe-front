44018020c3165f6973995d734d403c45f5b235a5
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-10-06 18:26:20 -0300

    Responsive allProducts

--- src/Pages/VerTodo.js
+++ src/Pages/VerTodo.js
@@ -3,10 +3,13 @@ import Products from '../Components/Products'
 import '../Styles/VerTodo.css'
 
 export default function VerTodo() {
-   
-    return (
-        <div className='ver-todo-page'>
-          <Products />
-        </div>
-    )
+
+  return (
+    <div className='ver-todo-page'>
+      <div className='allProductContain'>
+        <h2 className="allProduct">Todas los productos</h2>
+        <Products />
+      </div>
+    </div>
+  )
 }
