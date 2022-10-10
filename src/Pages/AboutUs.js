03f9035db72d8cdd7910776a9148711a6f9b8c28
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-10-10 13:27:27 -0300

    modification about us

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -11,28 +11,32 @@ export default function AboutUs() {
                 </div>
             </div>
             <div className='aboutUsContainer'>
-                <div className='aboutImg'>
-                    <div className='imgAboutcontainer'>
-                        <img className='imgAboutUs' src='aboutus.jpg' alt="cafe" />
+                <div className='contAbout'>
+                    <div className='aboutImg'>
+                        <div className='imgAboutcontainer'>
+                            <img className='imgAboutUs' src='aboutus4.jpg' alt="cafe" />
+                        </div>
                     </div>
-                </div>
-                <div className='aboutText'>
-                    <h2>Nuestra Historia</h2>
-                    <p>Desde 1933 tostamos nuestro propio café.
-Lo cuidamos desde la selección de sus granos, hasta la taza, conservando el amor y el respeto que tenemos por lo que hacemos, desde el primer día. Por eso nuestro café es ÚNICO.
-A través de nuestro proceso exclusivo de tostación, logramos resaltar las notas y características propias de los diferentes granos. Un aroma pleno y un sabor con cuerpo, con un toque de frescura, es lo que hace inconfundible a un .Cafe, siempre..</p>
+                    <div className='aboutText'>
+                        <h3>Nuestra Historia</h3>
+                        <p>Desde 1933 tostamos nuestro propio café.
+                            Lo cuidamos desde la selección de sus granos, hasta la taza, conservando el amor y el respeto que tenemos por lo que hacemos, desde el primer día. Por eso nuestro café es ÚNICO.
+                            A través de nuestro proceso exclusivo de tostación, logramos resaltar las notas y características propias de los diferentes granos. Un aroma pleno y un sabor con cuerpo, con un toque de frescura, es lo que hace inconfundible a un .Cafe, siempre..</p>
+                    </div>
                 </div>
             </div>
 
             <div className='aboutUsContainer1'>
-                <div className='aboutImg'>
-                    <div className='imgAboutcontainer'>
-                        <img className='imgAboutUs' src='aboutus3.jpg' alt="cafe" />
+                <div className='contAbout1'>
+                    <div className='aboutImg'>
+                        <div className='imgAboutcontainer'>
+                            <img className='imgAboutUs' src='aboutus5.jpg' alt="cafe" />
+                        </div>
                     </div>
-                </div>
-                <div className='aboutText'>
-                <h2>Nuestra Misión</h2>
-                    <p>Inspirar y nutrir el espíritu humano: Una persona, una taza y una comunidad a la vez.</p>
+                    <div className='aboutText'>
+                        <h3>Nuestra Misión</h3>
+                        <p>Inspirar y nutrir el espíritu humano: Una persona, una taza y una comunidad a la vez.</p>
+                    </div>
                 </div>
             </div>
             <div className='aboutUsBanner2'>
