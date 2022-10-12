e06601d6ec7415758c5cc0502e07cd11c2c4a6c6
Matias98cs <matias98cs@gmail.com>
2022-10-12 17:11:34 -0300

    admin y btn para borrar una orden

--- src/Components/OrderNroCard.js
+++ src/Components/OrderNroCard.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import { Link as LinkRouter } from 'react-router-dom'
-import '../Styles/OrderNroCard.css'
+import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import "../Styles/OrderNroCard.css";
 
-export default function OrderNroCard({order}) {
-    let date = new Date(order.date).toISOString().substring(0,10)
-    let myorder = '/mi-orden/'+ order?._id
+export default function OrderNroCard({ order }) {
+  let date = new Date(order.date).toISOString().substring(0, 10);
+  let myorder = "/mi-orden/" + order?._id;
 
-    return (
-        <>
-            <div className="OrderCardContainer">
-                <div className="OrderNumber">
-                    <p><span>Order: </span>#{order._id}</p>
-                    <p><span>Estado: </span>{order.payment ? "Pagado" : "Pendiente de pago"} - {order.state}</p>
-                </div>
-                <div className="OrderLink">
-                    <p>{date}</p>
-                    <LinkRouter to={`/mi-orden/${order?._id}`}><img src="/view-files.png" alt="order" /></LinkRouter>
-                </div>
-            </div>
-        </>
-    )
+  const handleRemoveItem = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <>
+      <div className="OrderCardContainer">
+        <div className="OrderNumber">
+          <p>
+            <span>Order: </span>#{order._id}
+          </p>
+          <p>
+            <span>Estado: </span>
+            {order.payment ? "Pagado" : "Pendiente de pago"} - {order.state}
+          </p>
+        </div>
+        <div className="OrderLink">
+          <p>{date}</p>
+          <div className="containerBtnOrder">
+            <LinkRouter to={`/mi-orden/${order?._id}`}>
+              <img src="/view-files.png" alt="order" />
+            </LinkRouter>
+            <button
+              onClick={() => handleRemoveItem(order._id)}
+              className="delete"
+            >
+              <img src="/delete-item.png" alt="delete" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
