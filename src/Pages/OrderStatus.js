15f7bfac5ecec50560f32af3326d1f9539594802
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-10-13 13:22:07 -0300

    fix order

--- src/Pages/OrderStatus.js
+++ src/Pages/OrderStatus.js
@@ -56,55 +56,60 @@ export default function OrderStatus() {
             <div className="orderContainer">
                 <div className='orderStatusContainer'>
                     <div className="paymentContainer">
-                    <div className='orderStatusText'>
+                        <div className='orderStatusText'>
+                            {
+                                status === "approved" ?
+                                    (<div>
+                                        <p className="titleOrder">Pago Aprobado</p>
+                                        <p><span className="titleSpan">Orden de compra:</span> #{lastOrderDetail?._id} </p>
+                                    </div>
+
+
+                                    ) : (<div className="noAprovedContainer">
+                                        <img className="imgOrder" src="http://drive.google.com/uc?export=view&id=1k4nHfXIUKxPJ91g8-SrD8Can6mXHB2yb" />
+                                        <p className="notFound-404">Pago Rechazado</p>
+                                    </div>)
+                            }
+                        </div>
+                        {status === "approved" ?
+                            lastOrderDetail?.products.map((item) => {
+                                return (
+                                    <div className='orderStatusProduct' key={item.item}>
+
+                                        <p><span className="titleSpan">Producto:</span> {item.name} </p>
+                                        <p>AR$ {item.totalPrice} </p>
+                                        <hr />
+                                    </div>
+
+                                )
+                            }) : null
+
+                        }
                         {
                             status === "approved" ?
-                                (<div>
-                                    <p className="titleOrder">Pago Aprobado</p>
-                                    <p><span className="titleSpan">Orden de compra:</span> #{lastOrderDetail?._id} </p>
-                                </div>
-
-                                ) : (<div>
-                                    <img src="http://drive.google.com/uc?export=view&id=1k4nHfXIUKxPJ91g8-SrD8Can6mXHB2yb" />
-                                    <p className="notFound-404">Pago Rechazado</p>
-                                </div>)
+                                (<div className='orderStatusText'>
+                                    <p>Su pago ha sido generado por el siguiente ticket:</p>
+                                    <p className="paymentID">#{paymentID}</p>
+                                    <p>Se enviará un resumen a:<span className="titleSpan"> {lastOrderDetail?.user.email} </span></p>
+                                </div>) : null
                         }
                     </div>
-                    {status === "approved" ?
-                        lastOrderDetail?.products.map((item) => {
-                            return (
-                                <div className='orderStatusProduct' key={item.item}>
-
-                                    <p><span className="titleSpan">Producto:</span> {item.name} </p>
-                                    <p>AR$ {item.totalPrice} </p>
-                                    <hr />
-                                </div>
-                            )
-                        }) : null
-
-                    }
                     {
                         status === "approved" ?
-                            (<div className='orderStatusText'>
-                                <p>Su pago ha sido generado por el siguiente ticket:</p>
-                                <p className="paymentID">#{paymentID}</p>
-                                <p>Se enviará un resumen a:<span className="titleSpan"> {lastOrderDetail?.user.email} </span></p>
-                            </div>) : null
-                    }
-                    </div>
-                <div className="shippingDataContainer">
-                <p className="titleOrder">Datos de Envio</p>
-                    <p><span className="titleSpan">Nombre:</span> {user?.firstName +" "+ user?.lastName}</p>
-                    <p><span className="titleSpan">Email:</span> {user?.email}</p>
-                    <p><span className="titleSpan">DNI:</span> {user?.dni}</p>
-                    <p><span className="titleSpan">Tel:</span> {user?.tel}</p>
-                    <p><span className="titleSpan">Dirección:</span> {user?.address}</p>
-                    <p><span className="titleSpan">CP:</span> {user?.zipCode}</p>
-                    <p><span className="titleSpan">Ciudad:</span> {user?.city}</p>
-                    <p><span className="titleSpan">Provincia:</span> {user?.province}</p>
+                            (<div className="shippingDataContainer">
+                                <p className="titleOrder">Datos de Envio</p>
+                                <p><span className="titleSpan">Nombre:</span> {user?.firstName + " " + user?.lastName}</p>
+                                <p><span className="titleSpan">Email:</span> {user?.email}</p>
+                                <p><span className="titleSpan">DNI:</span> {user?.dni}</p>
+                                <p><span className="titleSpan">Tel:</span> {user?.tel}</p>
+                                <p><span className="titleSpan">Dirección:</span> {user?.address}</p>
+                                <p><span className="titleSpan">CP:</span> {user?.zipCode}</p>
+                                <p><span className="titleSpan">Ciudad:</span> {user?.city}</p>
+                                <p><span className="titleSpan">Provincia:</span> {user?.province}</p>
+                            </div>): null
+                }
                 </div>
-                </div>
-                    <LinkRouter className="btnOrder" onClick={updateOrder} to={`/mi-cuenta/${userData?._id}`} >ir a mi cuenta</LinkRouter>
+                <LinkRouter className="btnOrder" onClick={updateOrder} to={`/mi-cuenta/${userData?._id}`} >ir a mi cuenta</LinkRouter>
             </div>
         </>
     )
