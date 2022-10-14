d368d38feb648c67b4c7531e152624e06b825a15
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-10-13 21:41:01 -0300

    uri api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
-//const apiURL = 'https://punto-cafe-backend.onrender.com'
-const apiURL = 'https://dot-caffe-backend.herokuapp.com/'
+// const apiURL = 'https://punto-cafe-backend.onrender.com'
 
-//const apiURL = 'http://localhost:4012'
+const apiURL = 'http://localhost:4012'
 
 export default apiURL
